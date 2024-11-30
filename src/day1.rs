8f0116fd23b51338fe70561b1c7c3daaffa93b21
Mattias Buelens <mattias@buelens.com>
2024-11-30 17:19:28 +0100

    Add template for day 1

---
+++ src/day1.rs
@@ -0,0 +1,35 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+type Input = Vec<String>;
+
+#[aoc_generator(day1)]
+fn parse(input: &str) -> Input {
+    todo!()
+}
+
+#[aoc(day1, part1)]
+fn part1(input: &Input) -> u32 {
+    todo!()
+}
+
+#[aoc(day1, part2)]
+fn part2(input: &Input) -> u32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        let example = "";
+        assert_eq!(part1(&parse(example)), todo!());
+    }
+
+    #[test]
+    fn part2_example() {
+        let example = "";
+        assert_eq!(part2(&parse(example)), todo!());
+    }
+}
