937502bd3d09b7ca9b2bdfcadc74a1123f43bdc0
Mattias Buelens <mattias@buelens.com>
2024-12-02 09:07:20 +0100

    Solve day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,5 +1,5 @@
-use crate::util::array_windows;
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
 
 type Input = Vec<Vec<i32>>;
 
@@ -11,11 +11,11 @@ fn parse(input: &str) -> Input {
         .collect()
 }
 
-fn is_safe(report: &[i32]) -> bool {
+fn is_safe<'a>(report: impl Iterator<Item = &'a i32>) -> bool {
     let mut increasing = true;
     let mut decreasing = true;
     let mut valid_distance = true;
-    for &[left, right] in array_windows::<i32, 2>(report) {
+    for (&left, &right) in report.tuple_windows() {
         increasing = increasing && left < right;
         decreasing = decreasing && left > right;
         valid_distance = valid_distance && (1..=3).contains(&(left - right).abs());
@@ -25,12 +25,26 @@ fn is_safe(report: &[i32]) -> bool {
 
 #[aoc(day2, part1)]
 fn part1(input: &Input) -> usize {
-    input.iter().filter(|report| is_safe(report)).count()
+    input.iter().filter(|report| is_safe(report.iter())).count()
+}
+
+fn is_safe_part2(report: &[i32]) -> bool {
+    if is_safe(report.iter()) {
+        // Safe without skipping any level
+        return true;
+    }
+    (0..report.len()).any(|i| {
+        // Skip the i-th level
+        let (left, right) = report.split_at(i);
+        let tolerated_report = left.iter().chain(right.iter().skip(1));
+        // Check if this is now safe
+        is_safe(tolerated_report)
+    })
 }
 
 #[aoc(day2, part2)]
 fn part2(input: &Input) -> usize {
-    todo!()
+    input.iter().filter(|report| is_safe_part2(report)).count()
 }
 
 #[cfg(test)]
@@ -51,6 +65,6 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse(EXAMPLE)), 0);
+        assert_eq!(part2(&parse(EXAMPLE)), 4);
     }
 }
