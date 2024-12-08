f2e3509e34f45e8e71e55b91074b68573842f35c
Mattias Buelens <mattias@buelens.com>
2024-12-08 13:01:26 +0100

    Solve day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -40,30 +40,56 @@ impl Map {
         (0..self.width).contains(&pos.x()) && (0..self.height).contains(&pos.y())
     }
 
-    fn antinodes(&self) -> impl Iterator<Item = Vector2D> + use<'_> {
+    fn antinodes(&self, part2: bool) -> impl Iterator<Item = Vector2D> + use<'_> {
         self.antennas
             .values()
-            .flat_map(|antennas| Map::antinodes_for(antennas))
-            .filter(|&antinode| self.is_in_bounds(antinode))
+            .flat_map(move |antennas| self.antinodes_for(antennas, part2))
+            .unique()
     }
 
-    fn antinodes_for(antennas: &[Vector2D]) -> impl Iterator<Item = Vector2D> + use<'_> {
-        antennas.iter().combinations(2).flat_map(|vec| {
+    fn antinodes_for<'a>(
+        &'a self,
+        antennas: &'a [Vector2D],
+        part2: bool,
+    ) -> impl Iterator<Item = Vector2D> + 'a {
+        antennas.iter().combinations(2).flat_map(move |vec| {
             let (left, right) = (*vec[0], *vec[1]);
             let offset = right - left;
-            [left - offset, right + offset]
+            let mut result = vec![];
+            if part2 {
+                let mut pos = left;
+                while self.is_in_bounds(pos) {
+                    result.push(pos);
+                    pos -= offset;
+                }
+                let mut pos = right;
+                while self.is_in_bounds(pos) {
+                    result.push(pos);
+                    pos += offset;
+                }
+            } else {
+                let left_antinode = left - offset;
+                if self.is_in_bounds(left_antinode) {
+                    result.push(left_antinode);
+                }
+                let right_antinode = right + offset;
+                if self.is_in_bounds(right_antinode) {
+                    result.push(right_antinode);
+                }
+            }
+            result
         })
     }
 }
 
 #[aoc(day8, part1)]
 fn part1(map: &Map) -> usize {
-    map.antinodes().unique().count()
+    map.antinodes(false).count()
 }
 
 #[aoc(day8, part2)]
 fn part2(map: &Map) -> usize {
-    todo!()
+    map.antinodes(true).count()
 }
 
 #[cfg(test)]
@@ -79,6 +105,6 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse(EXAMPLE)), 0);
+        assert_eq!(part2(&parse(EXAMPLE)), 34);
     }
 }
