24ee3205b85151a11e13dd3379d8ccb5e905fe4f
Mattias Buelens <mattias@buelens.com>
2024-12-09 21:04:53 +0100

    Solve day 9 part 2

--- src/day9.rs
+++ src/day9.rs
@@ -5,6 +5,7 @@ struct DiskSection {
     file_id: u64,
     start: u64,
     end: u64,
+    moved: bool,
 }
 
 #[derive(Debug, Clone)]
@@ -25,6 +26,7 @@ fn parse(input: &str) -> DiskMap {
                 file_id,
                 start: block_index,
                 end: block_index + size,
+                moved: false,
             });
             file_id += 1;
         }
@@ -35,8 +37,8 @@ fn parse(input: &str) -> DiskMap {
 }
 
 impl DiskMap {
-    fn compact(&mut self) -> &mut Self {
-        while let Some((index, free_start, free_amount)) = self.next_free_range() {
+    fn compact_blocks(&mut self) -> &mut Self {
+        while let Some((index, free_start, free_amount)) = self.next_free_range(1) {
             let last_section = self.sections.last_mut().unwrap();
             let last_file_id = last_section.file_id;
             let last_size = last_section.end - last_section.start;
@@ -46,6 +48,7 @@ impl DiskMap {
                     file_id: last_file_id,
                     start: free_start,
                     end: free_start + last_size,
+                    moved: true,
                 }
             } else {
                 last_section.end -= free_amount;
@@ -53,6 +56,7 @@ impl DiskMap {
                     file_id: last_file_id,
                     start: free_start,
                     end: free_start + free_amount,
+                    moved: true,
                 }
             };
             self.sections.insert(index, new_section);
@@ -60,12 +64,12 @@ impl DiskMap {
         self
     }
 
-    /// Find the range of free blocks, if any.
+    /// Find the next range of free blocks with at least `size` blocks, if any.
     /// Returns (index, start of range, number of free blocks)
-    fn next_free_range(&self) -> Option<(usize, u64, u64)> {
+    fn next_free_range(&self, min_size: u64) -> Option<(usize, u64, u64)> {
         let mut iter = self.sections.iter().enumerate().peekable();
         while let (Some((_, curr)), Some((next_index, next))) = (iter.next(), iter.peek()) {
-            if curr.end < next.start {
+            if curr.end + min_size <= next.start {
                 return Some((*next_index, curr.end, next.start - curr.end));
             }
         }
@@ -82,12 +86,44 @@ impl DiskMap {
 
 #[aoc(day9, part1)]
 fn part1(input: &DiskMap) -> u64 {
-    input.clone().compact().checksum()
+    input.clone().compact_blocks().checksum()
+}
+
+impl DiskMap {
+    fn compact_files(&mut self) -> &mut Self {
+        while let Some((index, file)) = self.next_unmoved_file() {
+            let size = file.end - file.start;
+            if let Some((free_index, free_start, _)) = self.next_free_range(size) {
+                // Must only move to the front
+                if free_index <= index {
+                    // Move the file
+                    let mut file = self.sections.remove(index);
+                    file.start = free_start;
+                    file.end = free_start + size;
+                    file.moved = true;
+                    self.sections.insert(free_index, file);
+                    continue;
+                }
+            }
+            // Not enough free space left for this file, skip it.
+            let file = &mut self.sections[index];
+            file.moved = true;
+        }
+        self
+    }
+
+    fn next_unmoved_file(&self) -> Option<(usize, &DiskSection)> {
+        self.sections
+            .iter()
+            .enumerate()
+            .rev()
+            .find(|(_, section)| !section.moved)
+    }
 }
 
 #[aoc(day9, part2)]
 fn part2(input: &DiskMap) -> u64 {
-    todo!()
+    input.clone().compact_files().checksum()
 }
 
 #[cfg(test)]
@@ -103,6 +139,6 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse(EXAMPLE)), 0);
+        assert_eq!(part2(&parse(EXAMPLE)), 2858);
     }
 }
