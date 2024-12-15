09bf0e90d36bacfb2b3be50450136d32127a5f09
Mattias Buelens <mattias@buelens.com>
2024-12-15 14:04:00 +0100

    Solve day 13 part 2

--- src/day13.rs
+++ src/day13.rs
@@ -8,6 +8,9 @@ struct Behavior {
     prize: Vector2D<i64>,
 }
 
+const COST_A: i64 = 3;
+const COST_B: i64 = 1;
+
 #[aoc_generator(day13)]
 fn parse(input: &str) -> Vec<Behavior> {
     fn parse_button(s: &str) -> Vector2D<i64> {
@@ -39,7 +42,7 @@ fn parse(input: &str) -> Vec<Behavior> {
 }
 
 impl Behavior {
-    fn solve(&self) -> Option<(i64, i64)> {
+    fn solve(&self) -> Option<i64> {
         // Ax * a + Bx * b = Px
         // Ay * a + By * b = Py
         //
@@ -54,39 +57,55 @@ impl Behavior {
             // Button A and button B are collinear
             // Use the cheapest button (B) to reach the prize
             let num_b_presses = self.prize.x() / self.button_b.x();
-            return if self.button_b * num_b_presses == self.prize {
-                Some((0, num_b_presses))
+            return if self.check(0, num_b_presses) {
+                Some(Self::cost(0, num_b_presses))
             } else {
-                // Prize is not collinear
                 None
             };
         }
         // b = (Ax * Py - Ay * Px) / determinant
-        let quotient = self.button_a.x().checked_mul(self.prize.y()).unwrap()
+        let quotient_b = self.button_a.x().checked_mul(self.prize.y()).unwrap()
             - self.button_a.y().checked_mul(self.prize.x()).unwrap();
-        if quotient % determinant != 0 {
+        if quotient_b % determinant != 0 {
             // Can't do half presses, this isn't Super Mario 64
             return None;
         }
-        let num_b_presses = quotient / determinant;
-        let num_a_presses =
-            (self.prize.x() - self.button_b.x() * num_b_presses) / self.button_a.x();
-        Some((num_a_presses, num_b_presses))
+        let num_b_presses = quotient_b / determinant;
+        // a = (Px - Bx * b) / Ax
+        let quotient_a = self.prize.x() - self.button_b.x().checked_mul(num_b_presses).unwrap();
+        if quotient_a % self.button_a.x() != 0 {
+            return None;
+        }
+        let num_a_presses = quotient_a / self.button_a.x();
+        assert!(self.check(num_a_presses, num_b_presses));
+        Some(Self::cost(num_a_presses, num_b_presses))
+    }
+
+    fn check(&self, num_a_presses: i64, num_b_presses: i64) -> bool {
+        self.button_a * num_a_presses + self.button_b * num_b_presses == self.prize
+    }
+
+    fn cost(num_a_presses: i64, num_b_presses: i64) -> i64 {
+        COST_A * num_a_presses + COST_B * num_b_presses
     }
 }
 
 #[aoc(day13, part1)]
 fn part1(input: &[Behavior]) -> i64 {
-    input
-        .iter()
-        .filter_map(Behavior::solve)
-        .map(|solution| 3 * solution.0 + 1 * solution.1)
-        .sum()
+    input.iter().filter_map(Behavior::solve).sum()
 }
 
 #[aoc(day13, part2)]
 fn part2(input: &[Behavior]) -> i64 {
-    todo!()
+    let new_input = input
+        .iter()
+        .cloned()
+        .map(|mut behavior| {
+            behavior.prize += Vector2D::new(10000000000000_i64, 10000000000000_i64);
+            behavior
+        })
+        .collect::<Vec<_>>();
+    part1(&new_input)
 }
 
 #[cfg(test)]
@@ -102,6 +121,6 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse(EXAMPLE)), 0);
+        assert_eq!(part2(&parse(EXAMPLE)), 875318608908);
     }
 }
