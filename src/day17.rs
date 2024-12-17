0ea93a98039ec9b43ee3c3b9474bfa25e6cbf683
Mattias Buelens <mattias@buelens.com>
2024-12-17 20:43:38 +0100

    Try to brute force day 17 part 2

--- src/day17.rs
+++ src/day17.rs
@@ -3,7 +3,7 @@ use itertools::Itertools;
 
 #[derive(Debug, Clone)]
 struct Program {
-    registers: [i32; 3],
+    registers: [u64; 3],
     pc: usize,
     code: Vec<u8>,
 }
@@ -11,16 +11,16 @@ struct Program {
 #[aoc_generator(day17)]
 fn parse(input: &str) -> Program {
     let (registers, program) = input.split_once("\n\n").unwrap();
-    let values: [i32; 3] = registers
+    let values: [u64; 3] = registers
         .lines()
         .zip('A'..='C')
         .map(|(line, name)| {
             let line = line.strip_prefix("Register ").unwrap();
             let line = line.strip_prefix(name).unwrap();
             let line = line.strip_prefix(": ").unwrap();
-            line.parse::<i32>().unwrap()
+            line.parse::<u64>().unwrap()
         })
-        .collect::<Vec<i32>>()
+        .collect::<Vec<u64>>()
         .try_into()
         .unwrap();
     let code = program.strip_prefix("Program: ").unwrap().trim_end();
@@ -62,42 +62,62 @@ impl TryFrom<u8> for Opcode {
 }
 
 impl Program {
-    fn run(&mut self) -> Vec<i32> {
-        let mut out = Vec::new();
+    fn run(&mut self) -> Vec<u64> {
+        let mut output = Vec::new();
         while self.pc + 1 < self.code.len() {
-            self.step(&mut out);
+            if let Some(value) = self.step() {
+                output.push(value);
+            }
         }
-        out
+        output
     }
 
-    fn step(&mut self, out: &mut Vec<i32>) {
+    fn run_while_matching(&mut self) -> bool {
+        let mut output_index = 0;
+        while self.pc + 1 < self.code.len() {
+            if let Some(value) = self.step() {
+                if value != self.code[output_index] as u64 {
+                    return false;
+                }
+                output_index += 1;
+            }
+        }
+        output_index == self.code.len()
+    }
+
+    fn step(&mut self) -> Option<u64> {
         let opcode = Opcode::try_from(self.code[self.pc]).expect("invalid opcode");
         let operand = self.code[self.pc + 1];
+        self.pc += 2;
         match opcode {
             Opcode::Adv => self.registers[0] >>= self.combo(operand),
-            Opcode::Bxl => self.registers[1] ^= operand as i32,
+            Opcode::Bxl => self.registers[1] ^= operand as u64,
             Opcode::Bst => self.registers[1] = self.combo(operand) & 0b111,
             Opcode::Jnz => {
                 if self.registers[0] != 0 {
                     self.pc = operand as usize;
-                    return;
                 }
             }
             Opcode::Bxc => self.registers[1] ^= self.registers[2],
-            Opcode::Out => out.push(self.combo(operand) & 0b111),
+            Opcode::Out => return Some(self.combo(operand) & 0b111),
             Opcode::Bdv => self.registers[1] = self.registers[0] >> self.combo(operand),
             Opcode::Cdv => self.registers[2] = self.registers[0] >> self.combo(operand),
         }
-        self.pc += 2;
+        None
     }
 
-    fn combo(&self, operand: u8) -> i32 {
+    fn combo(&self, operand: u8) -> u64 {
         match operand {
-            0..=3 => operand as i32,
+            0..=3 => operand as u64,
             4..=6 => self.registers[(operand - 4) as usize],
             7 => panic!("reserved"),
             _ => panic!("invalid combo operand {operand}"),
         }
+    }
+
+    fn reset(&mut self, program: &Program) {
+        self.registers = program.registers;
+        self.pc = 0;
     }
 }
 
@@ -107,23 +127,35 @@ fn part1(program: &Program) -> String {
 }
 
 #[aoc(day17, part2)]
-fn part2(program: &Program) -> String {
-    todo!()
+fn part2(program: &Program) -> u64 {
+    let mut new_program = program.clone();
+    for a in 0u64.. {
+        if a % 10_000_000 == 0 {
+            dbg!(a);
+        }
+        new_program.reset(program);
+        new_program.registers[0] = a;
+        if new_program.run_while_matching() {
+            return a;
+        }
+    }
+    panic!("no value found for A");
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = include_str!("../examples/2024/day17.txt");
+    const EXAMPLE1: &str = include_str!("../examples/2024/day17-1.txt");
+    const EXAMPLE2: &str = include_str!("../examples/2024/day17-2.txt");
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&parse(EXAMPLE)), "4,6,3,5,6,3,5,2,1,0");
+        assert_eq!(part1(&parse(EXAMPLE1)), "4,6,3,5,6,3,5,2,1,0");
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse(EXAMPLE)), "");
+        assert_eq!(part2(&parse(EXAMPLE2)), 117440);
     }
 }
