e3347689c1b93a177788efd7238e48cb1f11a4df
Mattias Buelens <mattias@buelens.com>
2024-12-22 21:22:29 +0100

    Solve day 22 part 2... very slowly

--- src/day22.rs
+++ src/day22.rs
@@ -1,4 +1,6 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
+use std::collections::HashSet;
 
 #[aoc_generator(day22)]
 fn parse(input: &str) -> Vec<u64> {
@@ -32,16 +34,63 @@ fn part1(input: &[u64]) -> u64 {
         .sum()
 }
 
+fn prices(secret: u64) -> impl Iterator<Item = (u64, i64)> {
+    secrets(secret)
+        .map(|secret| secret % 10)
+        .tuple_windows()
+        .map(|(prev_price, price)| {
+            let change = price as i64 - prev_price as i64;
+            (price, change)
+        })
+}
+
+type Change = [i64; 4];
+
+fn price_changes(secret: u64) -> impl Iterator<Item = (u64, Change)> {
+    prices(secret).tuple_windows().map(
+        |((_, change1), (_, change2), (_, change3), (price, change4))| {
+            (price, [change1, change2, change3, change4])
+        },
+    )
+}
+
+fn compute_total_bananas(change: &Change, all_changes: &[Vec<(u64, Change)>]) -> u64 {
+    let mut total = 0;
+    for monkey_changes in all_changes {
+        if let Some((price, _)) = monkey_changes.iter().find(|(_, c)| change == c) {
+            total += price;
+        }
+    }
+    total
+}
+
 #[aoc(day22, part2)]
 fn part2(input: &[u64]) -> u64 {
-    todo!()
+    let all_changes = input
+        .iter()
+        .map(|&secret| price_changes(secret).take(2000 + 1).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let mut most_bananas = 0;
+    let mut seen = HashSet::<&Change>::new();
+    for changes in &all_changes {
+        for (_, change) in changes {
+            if seen.contains(change) {
+                continue;
+            }
+            let total = compute_total_bananas(change, &all_changes);
+            seen.insert(change);
+            if total > most_bananas {
+                dbg!(total, change);
+                most_bananas = total;
+            }
+        }
+    }
+    most_bananas
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    const EXAMPLE: &str = include_str!("../examples/2024/day22.txt");
 
     #[test]
     fn step_example() {
@@ -56,12 +105,33 @@ mod tests {
     }
 
     #[test]
+    fn prices_example() {
+        let values = prices(123).take(9).collect::<Vec<_>>();
+        assert_eq!(
+            values,
+            vec![
+                (0, -3),
+                (6, 6),
+                (5, -1),
+                (4, -1),
+                (4, 0),
+                (6, 2),
+                (4, -2),
+                (4, 0),
+                (2, -2),
+            ]
+        );
+    }
+
+    #[test]
     fn part1_example() {
-        assert_eq!(part1(&parse(EXAMPLE)), 37327623);
+        let input = vec![1, 10, 100, 2024];
+        assert_eq!(part1(&input), 37327623);
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse(EXAMPLE)), 0);
+        let input = vec![1, 2, 3, 2024];
+        assert_eq!(part2(&input), 23);
     }
 }
