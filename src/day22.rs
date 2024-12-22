31ad842cef625ac78dd0d345e9de4485a1552a9e
Mattias Buelens <mattias@buelens.com>
2024-12-22 20:54:51 +0100

    Tweak

--- src/day22.rs
+++ src/day22.rs
@@ -16,10 +16,11 @@ fn mix_and_prune(value: u64, secret: u64) -> u64 {
     (value ^ secret) % 16777216
 }
 
-fn secret_sequence(mut secret: u64) -> impl Iterator<Item = u64> {
+fn secrets(mut secret: u64) -> impl Iterator<Item = u64> {
     std::iter::from_fn(move || {
+        let current = secret;
         secret = secret_step(secret);
-        Some(secret)
+        Some(current)
     })
 }
 
@@ -27,7 +28,7 @@ fn secret_sequence(mut secret: u64) -> impl Iterator<Item = u64> {
 fn part1(input: &[u64]) -> u64 {
     input
         .iter()
-        .map(|&secret| secret_sequence(secret).nth(2000 - 1).unwrap())
+        .map(|&secret| secrets(secret).nth(2000).unwrap())
         .sum()
 }
 
@@ -44,12 +45,12 @@ mod tests {
 
     #[test]
     fn step_example() {
-        let values = secret_sequence(123).take(10).collect::<Vec<_>>();
+        let values = secrets(123).skip(1).take(10).collect::<Vec<_>>();
         assert_eq!(
             values,
             vec![
-                15887950, 16495136, 527345, 704524, 1553684, 12683156, 11100544, 12249484, 7753432,
-                5908254,
+                15887950, 16495136, 527345, 704524, 1553684, //
+                12683156, 11100544, 12249484, 7753432, 5908254,
             ]
         );
     }
